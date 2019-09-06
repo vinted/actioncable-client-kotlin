128f56aeaf461fc2ccbf10bd954b9ea0d05232ac
GediminasZukas <gediminas.zukas@vinted.com>
2019-09-06 15:49:18 +0300

    Bump version to 0.3.3

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -16,7 +16,7 @@ object Versions {
 
     private const val MAJOR = 0
     private const val MINOR = 3
-    private const val PATCH = 0
+    private const val PATCH = 3
 
     const val VERSION_CODE: Int = (MAJOR * 10000) + (MINOR * 100) + PATCH
     const val VERSION_NAME: String = "$MAJOR.$MINOR.$PATCH"
