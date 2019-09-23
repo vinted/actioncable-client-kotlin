9633e5f295c4133f23aec52d40a38cd698e1973b
GediminasZukas <gediminas.zukas@vinted.com>
2019-09-23 17:49:24 +0300

    Update mockito kotlin 1.5.0 -> 2.1.0

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -6,7 +6,7 @@ object Versions {
     const val JUNIT = "5.4.2"
     const val ROBOLECTRIC = "3.7.1"
     const val MOCKITO = "2.13.0"
-    const val MOCKITO_KOTILN = "1.5.0"
+    const val MOCKITO_KOTILN = "2.1.0"
     const val GSON = "2.8.4"
     const val OKHTTP = "3.12.1"
 
@@ -30,7 +30,7 @@ object Libs {
     const val MOCKWEBSERVER = "com.squareup.okhttp3:mockwebserver:${Versions.OKHTTP}"
     const val ROBOLECTRIC = "org.robolectric:robolectric:${Versions.ROBOLECTRIC}"
     const val MOCKITO = "org.mockito:mockito-core:${Versions.MOCKITO}"
-    const val MOCKITO_KOTLIN = "com.nhaarman:mockito-kotlin:${Versions.MOCKITO_KOTILN}"
+    const val MOCKITO_KOTLIN = "com.nhaarman.mockitokotlin2:mockito-kotlin:${Versions.MOCKITO_KOTILN}"
     const val GSON = "com.google.code.gson:gson:${Versions.GSON}"
     const val OKHTTP = "com.squareup.okhttp3:okhttp:${Versions.OKHTTP}"
 }
